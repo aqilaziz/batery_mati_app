6418a6e34b41f11359e4440a0f6abbd345f6b912
Aqil Aziz <aqilaziz@users.noreply.github.com>
2026-06-24 08:58:37 +0800

    release: v8 APK - version bump 1.7
    Merged overlay-before-notification fix + version 8/1.7 build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
   alias(libs.plugins.secrets)
 }
 
-val appVersionCode = 7
-val appVersionName = "1.6"
+val appVersionCode = 8
+val appVersionName = "1.7"
 
 android {
   namespace = "com.example"
